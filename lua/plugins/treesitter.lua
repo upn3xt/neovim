e6303a5eb645c513eceae4110760d3603b2afd88
upn3xt <whonextmail25@gmail.com>
2026-04-25 14:32:57 +0100

    fixes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,19 +5,6 @@ return {
   branch = 'main',
 
   config = function()
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-    parser_config.c3 = {
-      install_info = {
-        url = "https://github.com/c3lang/tree-sitter-c3",
-        files = { "src/parser.c",
-          "src/scanner.c",
-        },
-        branch = "main",
-      },
-      filetype = "c3",
-    }
-
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
         "lua",
